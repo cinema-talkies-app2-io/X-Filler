27532805ff272149e51e75c33c40f2ecf85c0b17
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-08-27 21:31:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -87,11 +87,11 @@ async def start(client, message):
                 
                  InlineKeyboardButton('✇ Sʜᴀʀᴇ Oɴ WʜᴀᴛsAᴘᴘ ✇', url="https://bit.ly/3LjsIsR")
                  ]]
-                 reply_markup = InlineKeyboardMarkup(buttons)
-                 m=await message.reply_sticker("CAACAgUAAxkBAAIGBGaIQ3GTvjPRwI1B_lFMKU-SFBSqAAIhAAPBJDExrJTo8r6ffCUeBA") 
-                 await asyncio.sleep(1)
-                 await m.delete()
-                 await message.reply_photo(
+            reply_markup = InlineKeyboardMarkup(buttons)
+            m=await message.reply_sticker("CAACAgUAAxkBAAIGBGaIQ3GTvjPRwI1B_lFMKU-SFBSqAAIhAAPBJDExrJTo8r6ffCUeBA") 
+            await asyncio.sleep(1)
+            await m.delete()
+            await message.reply_photo(
                  photo=random.choice(PICS),
                  caption=script.START_TXT1.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
                  reply_markup=reply_markup,
@@ -124,11 +124,11 @@ async def start(client, message):
                 
                  InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
                  ]]
-                 reply_markup = InlineKeyboardMarkup(buttons)
-                 m=await message.reply_sticker("CAACAgUAAxkBAAIGBGaIQ3GTvjPRwI1B_lFMKU-SFBSqAAIhAAPBJDExrJTo8r6ffCUeBA") 
-                 await asyncio.sleep(1)
-                 await m.delete()
-                 await message.reply_photo(
+             reply_markup = InlineKeyboardMarkup(buttons)
+             m=await message.reply_sticker("CAACAgUAAxkBAAIGBGaIQ3GTvjPRwI1B_lFMKU-SFBSqAAIhAAPBJDExrJTo8r6ffCUeBA") 
+             await asyncio.sleep(1)
+             await m.delete()
+             await message.reply_photo(
                  photo=random.choice(PICS),
                  caption=script.START_TXT2.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
                  reply_markup=reply_markup,
