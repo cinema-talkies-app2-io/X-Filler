01defd42b437b31d035f336428451edb9b93494e
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-08-27 14:44:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -117,19 +117,17 @@ async def start(client, message):
     ) 
       if len(message.command) != 2:
          if PREMIUM_AND_REFERAL_MODE == True:
+             user_id = message.from_user.id
+                 ABCDE = "https://t.me/share/url?url=https://telegram.me/File_Search_RoBot?start=X-{}".format(user_id)
+                 ABCD = "https://wa.me/?text=https://telegram.me/File_Search_RoBot?start=X-{}".format(user_id)
+        
+            
             buttons = [[
-                InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-                InlineKeyboardButton('Eᴀʀɴ Mᴏɴᴇʏ 💸', callback_data="shortlink_info"),
-                InlineKeyboardButton('⌬ Mᴏᴠɪᴇ Gʀᴏᴜᴘ', url=GRP_LNK)
-            ],[
-                InlineKeyboardButton('〄 Hᴇʟᴘ', callback_data='help'),
-                InlineKeyboardButton('⍟ Aʙᴏᴜᴛ', callback_data='about')
-            ],[
-                InlineKeyboardButton('🔻 ɢᴇᴛ ғʀᴇᴇ/ᴘᴀɪᴅ sᴜʙsᴄʀɪᴘᴛɪᴏɴ 🔻', callback_data='subscription')
-            ],[
-                InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
-            ]]
+                
+                 InlineKeyboardButton('💫 Rᴇғᴇʀ ᴏɴ WʜᴀᴛsAᴘᴘ 💫', url=ABCD)],[
+                 InlineKeyboardButton('💫 Rᴇғᴇʀ ᴏɴ Tᴇʟᴇɢʀᴀᴍ 💫', url=ABCDE)
+            
+                 ]]       
          else:
             buttons = [[
                 
