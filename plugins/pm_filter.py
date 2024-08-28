6b44a601715c3e8467a9e416b1912fdff035636e
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-08-29 00:03:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1947,7 +1947,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await asyncio.sleep(1)
             await xo.delete()
 
-            await log_msg.reply_text(
+            yo=await log_msg.reply_text(
                 text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n•• ᴜꜱᴇʀɴᴀᴍᴇ : {username} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᗴ : {fileName}",
                 quote=True,
                 disable_web_page_preview=True,
@@ -1968,6 +1968,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
             )
             # Wait for 120 seconds
             await asyncio.sleep(86400)
+            await yo.delete()
+            await log_msg.delete()
+
+
 
 # Delete the message
             await msg.delete()   
