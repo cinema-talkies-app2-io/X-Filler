dfc625f8022cafc5c05a661472905e7d215081cf
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-09-16 10:35:02 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -228,18 +228,18 @@ async def start(client, message):
         user_id = int(data.split("-", 1)[1])
 
     # Check if the referring user is already in the database
-        user_exists = await db.is_user_exist(user_id)
+        user_exists = await db.is_userz_exist(user_id)
     
         if user_exists:
         # Notify the user that they have already referred
             await message.reply("<b>You have already registered </b>")
         else:
-          #   await db.add_userz(message.from_user.id, message.from_user.first_name)
-        # Process the referral
              vj = await referal_add_user(user_id, message.from_user.id)
         
              if vj and PREMIUM_AND_REFERAL_MODE == True:
                  await message.reply(f"<b>You have joined using the referral link of user with ID {user_id}\n\nSend /start again to use the bot</b>")
+                 await db.add_userz(message.from_user.id, message.from_user.first_name)
+        # Process the referral
              
                  num_referrals = await get_referal_users_count(user_id)
                  await client.send_message(chat_id=user_id, text=f"<b>{message.from_user.mention} started the bot with your referral link\n\nTotal Referrals - {num_referrals}</b>")
